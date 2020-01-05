390d276d15785890eac11f4b7872ba02b2da9bf2
ridwansalawu <reedysalawu@gmail.com>
2020-01-06 01:19:02 +1100

    first graph sketch

--- client/src/components/Partograph.js
+++ client/src/components/Partograph.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
-import './partograph.css'
+import './partograph.css';
+import _ from "lodash";
 
 class Partograph extends Component{
 
@@ -14,8 +15,50 @@ class Partograph extends Component{
 
     componentDidMount() {
         console.log("i'v mounted")
+        const drawingBoardWidth = 800;
+        const drawingBoardHeight = 400;
         let mains = d3.select(".main-page")
-        console.log(mains);
+        // customize the xrange input
+        let xRange = _.range(25) 
+        let yRange = _.range(11)
+  
+        
+        const drawingBoard = mains
+
+                             .append("svg")
+                             .attr("id", "svgg")
+                             .attr("width",  "90%")
+                             .attr("height", "90%")
+                            //  .style("border", "black 2px solid")
+
+        const xScale = d3.scaleLinear()
+                         .domain(d3.extent(xRange))
+                         .range([0, drawingBoardWidth])
+
+        const xAxis = d3.axisBottom()
+                        .scale(xScale)
+                        .ticks(24)
+                        .tickSize(-400)
+                        
+
+        drawingBoard.append("g")
+            .attr("transform", "translate(20,400)")
+            .call(xAxis)
+            
+
+        const yScale = d3.scaleLinear()
+            .domain(d3.extent(yRange))
+            .range([drawingBoardHeight, 0])
+        
+
+        const yAxis = d3.axisLeft()
+            .scale(yScale)
+            .tickSize(-800)
+
+        drawingBoard.append("g")
+            .attr("transform", "translate(20, 0)")
+            .call(yAxis)
+            
     }
     
 
