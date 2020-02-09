fd51829d97dd01a750978473c205b532641add63
ridwansalawu <reedysalawu@gmail.com>
2020-02-09 17:46:50 +1100

    socket io heroku

--- frontend/src/components/InfoBar/InfoBar.js
+++ frontend/src/components/InfoBar/InfoBar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import onlineIcon from '../icons/onlineIcon.png';
-import closeIcon from '../icons/closeIcon.png';
+// import onlineIcon from '../icons/onlineIcon.png';
+// import closeIcon from '../icons/closeIcon.png';
 
 import './InfoBar.css';
 
 const InfoBar = ({ room }) => (
   <div className="infoBar">
     <div className="leftInnerContainer">
-      <img className="onlineIcon" src={onlineIcon} alt="online icon" />
+      {/* <img className="onlineIcon" src={onlineIcon} alt="online icon" /> */}
       <h3>{room}</h3>
     </div>
     <div className="rightInnerContainer">
